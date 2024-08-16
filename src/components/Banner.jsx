9f85812c4d9c308788e571ff6469c93ac1984300
Sagor Hossain <sagor.cnits@gmail.com>
2024-08-16 08:20:56 +0600

    banner section title chabges

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,11 +12,9 @@ const Banner = () => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
-          <h1 className="mb-5 text-2xl md:text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-2xl md:text-4xl font-bold">Find Your Product</h1>
           <p className="mb-5 text-sm">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+          You find the product you like
           </p>
           <div className="flex gap-2 justify-between items-center bg-white  overflow-hidden text-black rounded-md">
             <input
